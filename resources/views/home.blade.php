7a7f22a2d047de4c6be841d5e530a41e331e29c7
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2018-10-04 14:19:00 +0200

    adding strip tags in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -49,7 +49,7 @@
                             <img class="card-img-top" src="{{ ($blog->cover == NULL) ? '/img/no-cover.png' : 'storage/cover_pics/' . $blog->cover }}" alt="Card image cap">
                             <div class="card-body">
                                 <h4 class="card-title">{{ $blog->title }}</h4>
-                                <p class="card-text">{{ str_limit($blog->content, rand(120,150)) }}</p>
+                                <p class="card-text">{{ strip_tags(str_limit($blog->content, rand(120,150))) }}</p>
                                 <small>posted by {{ $blog->firstName }} {{ $blog->lastName }} {{Carbon\Carbon::parse($blog->created_at)->diffForHumans()}}</small>
                                 <hr>
                                 <a href="blogs/{{$blog->id}}" class="btn btn-orange">Read More &raquo;</a>
